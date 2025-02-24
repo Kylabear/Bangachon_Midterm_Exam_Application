46202df3e0a307374c61859ba5833ff725ff2ee7
kyla <116809759+Kylabear@users.noreply.github.com>
2025-02-24 13:32:13 +0000

    Added ProductController with products array

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,18 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = [
+            ['name' => 'iPhone 15', 'price' => 1099, 'brand' => 'Apple'],
+            ['name' => 'Galaxy S23', 'price' => 999, 'brand' => 'Samsung'],
+            ['name' => 'Pixel 7', 'price' => 899, 'brand' => 'Google']
+        ];
+
+        return view('products.index', compact('products'));
+    }
 }
